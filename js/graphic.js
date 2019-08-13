2eed7ddaf43663719cfb6559e04d355551a9e351
klosha <40450203+klosha@users.noreply.github.com>
2019-08-14 03:18:37 +1000

    Update graphic.js

--- js/graphic.js
+++ js/graphic.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
       "order": [
         [10, "asc"]
       ],
-      "pagingType": "advanced" //no page numbers
+      "pagingType": "full_numbers"
         //uncomment these options to simplify your table
         //"paging": false,
         //"searching": false,
